54ed135ced57489dfc140b5fa2e3892b68ad91f6
Chris Hendrix <chendrix1123@gmail.com>
2023-02-04 09:36:48 -0500

    move next-auth to global

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,14 @@
 /* eslint-disable no-var */
 /* eslint-disable vars-on-top */
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
+import NextAuth from 'next-auth'
 
 declare global {
   var prisma: PrismaClient | null
+}
+
+declare module 'next-auth' {
+  export interface Session extends NextAuth.Session {
+    user: User | null
+  }
 }
--- next-auth.d.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from 'next-auth'
-import { User } from '@prisma/client'
-
-declare module 'next-auth' {
-  export interface Session extends NextAuth.Session {
-    user: User | null
-  }
-}
